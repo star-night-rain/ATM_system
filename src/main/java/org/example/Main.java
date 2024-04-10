93c936aaf2f68b3dc57d8c9b35b7c05dd96e8a90
star-night-rain <1747432970.@qq.com>
2024-04-10 15:07:40 +0800

    关闭时发生"BYE";只进行一次连接

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main
 {
-    static String ip = "127.0.0.1";
+    static String ip = "10.234.107.98";
     static int port = 2525;
     public static void main(String[] args)
     {
--- src/main/java/org/example/TCPClient.java
+++ src/main/java/org/example/TCPClient.java
@@ -52,8 +52,8 @@ public class TCPClient extends JFrame
     JPanel panel4 = new JPanel(new BorderLayout());
     JPanel panel5 = new JPanel(new BorderLayout());
 
-    Font font = new Font("宋体",Font.BOLD,18);
-    Font font2 = new Font("宋体",Font.BOLD,22);
+    Font font = new Font("宋体", Font.BOLD, 18);
+    Font font2 = new Font("宋体", Font.BOLD, 22);
     JButton button1 = new JButton("提交");
 
     JButton button2 = new JButton("登录");
@@ -65,7 +65,8 @@ public class TCPClient extends JFrame
     JTextField text_of_balance = new JTextField();
     JButton button1_of_balance = new JButton("确认取款");
     JButton button2_of_balance = new JButton("返回");
-    public TCPClient(String ip,int port)
+
+    public TCPClient(String ip, int port)
     {
         this.ip = ip;
         this.port = port;
@@ -77,25 +78,21 @@ public class TCPClient extends JFrame
         setLayout3();
         setLayout4();
         setLayout5();
+        connect();
 
-        button1.addActionListener(new ActionListener() {
+        button1.addActionListener(new ActionListener()
+        {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
+            public void actionPerformed(ActionEvent e)
+            {
+                try
+                {
                     account = text_of_account.getText();
                     System.out.println(account);
 
-                    try
-                    {
-                        clientSocket = new Socket(ip, port);
-                        writer = new DataOutputStream(clientSocket.getOutputStream());
-                        reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    } catch (IOException x)
-                    {
-                        throw new RuntimeException(x);
-                    }
+
                     // 发送账户信息
-                    writer.writeBytes("HELO "+account+'\n');
+                    writer.writeBytes("HELO " + account + '\n');
 
                     // 读取服务器响应状态
 
@@ -103,42 +100,46 @@ public class TCPClient extends JFrame
 
                     System.out.println("Server Response: " + status);
 
-                    writer.close();
-                    reader.close();
-                    clientSocket.close();
+//                    writer.close();
+//                    reader.close();
+//                    clientSocket.close();
 
-                    if(Objects.equals(status, "500 AUTH REQUIRED"))
+                    if (Objects.equals(status, "500 AUTH REQUIRED"))
                     {
                         panel1.setVisible(false);
                         panel2.setVisible(true);
                     }
                     else
-                        JOptionPane.showMessageDialog(null,"您输入的账号不合法！","登录失败",JOptionPane.ERROR_MESSAGE);
+                        JOptionPane.showMessageDialog(null, "您输入的账号不存在！", "登录失败", JOptionPane.ERROR_MESSAGE);
 
-                } catch (IOException ex) {
+                } catch (IOException ex)
+                {
                     // 捕获并处理异常
                     ex.printStackTrace();
                 }
             }
         });
 
-        button2.addActionListener(new ActionListener() {
+        button2.addActionListener(new ActionListener()
+        {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
+            public void actionPerformed(ActionEvent e)
+            {
+                try
+                {
                     password = text_of_password.getText();
                     System.out.println(password);
-                    try
-                    {
-                        clientSocket = new Socket(ip, port);
-                        writer = new DataOutputStream(clientSocket.getOutputStream());
-                        reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    } catch (IOException x)
-                    {
-                        throw new RuntimeException(x);
-                    }
+//                    try
+//                    {
+//                        clientSocket = new Socket(ip, port);
+//                        writer = new DataOutputStream(clientSocket.getOutputStream());
+//                        reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//                    } catch (IOException x)
+//                    {
+//                        throw new RuntimeException(x);
+//                    }
                     // 发送账户信息
-                    writer.writeBytes("PASS "+password+'\n');
+                    writer.writeBytes("PASS " + password + '\n');
 
                     // 读取服务器响应状态
 
@@ -146,18 +147,19 @@ public class TCPClient extends JFrame
 
                     System.out.println("Server Response: " + status);
 
-                    writer.close();
-                    reader.close();
-                    clientSocket.close();
+//                    writer.close();
+//                    reader.close();
+//                    clientSocket.close();
 
-                    if(Objects.equals(status, "525 OK!"))
+                    if (Objects.equals(status, "525 OK!"))
                     {
                         panel2.setVisible(false);
                         panel3.setVisible(true);
                     }
                     else
-                        JOptionPane.showMessageDialog(null,"您输入的密码不正确！","登录失败",JOptionPane.ERROR_MESSAGE);
-                } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(null, "您输入的密码不正确！", "登录失败", JOptionPane.ERROR_MESSAGE);
+                } catch (IOException ex)
+                {
                     // 捕获并处理异常
                     ex.printStackTrace();
                 }
@@ -167,19 +169,21 @@ public class TCPClient extends JFrame
         button3.addActionListener(new ActionListener()
         {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    try
-                    {
-                        clientSocket = new Socket(ip, port);
-                        writer = new DataOutputStream(clientSocket.getOutputStream());
-                        reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    } catch (IOException x)
-                    {
-                        throw new RuntimeException(x);
-                    }
+            public void actionPerformed(ActionEvent e)
+            {
+                try
+                {
+//                    try
+//                    {
+//                        clientSocket = new Socket(ip, port);
+//                        writer = new DataOutputStream(clientSocket.getOutputStream());
+//                        reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//                    } catch (IOException x)
+//                    {
+//                        throw new RuntimeException(x);
+//                    }
                     // 发送账户信息
-                    writer.writeBytes("BALA"+'\n');
+                    writer.writeBytes("BALA" + '\n');
 
                     // 读取服务器响应状态
 
@@ -188,21 +192,23 @@ public class TCPClient extends JFrame
                     setTitle("余额查询");
                     label_of_check.setText(balance);
 
-                    writer.close();
-                    reader.close();
-                    clientSocket.close();
+//                    writer.close();
+//                    reader.close();
+//                    clientSocket.close();
                     setTitle("操作界面");
                     panel3.setVisible(false);
                     panel4.setVisible(true);
 
-                } catch (IOException ex) {
+                } catch (IOException ex)
+                {
                     // 捕获并处理异常
                     ex.printStackTrace();
                 }
             }
         });
 
-        button4.addActionListener(new ActionListener() {
+        button4.addActionListener(new ActionListener()
+        {
             @Override
             public void actionPerformed(ActionEvent e)
             {
@@ -214,9 +220,29 @@ public class TCPClient extends JFrame
 
         });
 
-        button5.addActionListener(new ActionListener() {
+        button5.addActionListener(new ActionListener()
+        {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e)
+            {
+
+                try
+                {
+                    writer.writeBytes("BYE\n");
+
+                    String status = reader.readLine();
+                    System.out.println("Server Response: " + status);
+
+                    writer.close();
+                    reader.close();
+                    clientSocket.close();
+
+
+                } catch (IOException ex)
+                {
+                    throw new RuntimeException(ex);
+                }
+
                 dispose();
             }
         });
@@ -237,21 +263,23 @@ public class TCPClient extends JFrame
         button1_of_balance.addActionListener(new ActionListener()
         {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
+            public void actionPerformed(ActionEvent e)
+            {
+                try
+                {
                     balance = text_of_balance.getText();
                     System.out.println(balance);
-                    try
-                    {
-                        clientSocket = new Socket(ip, port);
-                        writer = new DataOutputStream(clientSocket.getOutputStream());
-                        reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    } catch (IOException x)
-                    {
-                        throw new RuntimeException(x);
-                    }
+//                    try
+//                    {
+//                        clientSocket = new Socket(ip, port);
+//                        writer = new DataOutputStream(clientSocket.getOutputStream());
+//                        reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//                    } catch (IOException x)
+//                    {
+//                        throw new RuntimeException(x);
+//                    }
                     // 发送账户信息
-                    writer.writeBytes("WDRA "+balance+'\n');
+                    writer.writeBytes("WDRA " + balance + '\n');
 
                     // 读取服务器响应状态
 
@@ -259,15 +287,16 @@ public class TCPClient extends JFrame
 
                     System.out.println("Server Response: " + status);
 
-                    writer.close();
-                    reader.close();
-                    clientSocket.close();
+//                    writer.close();
+//                    reader.close();
+//                    clientSocket.close();
 
-                    if(Objects.equals(status, "525 OK"))
-                        JOptionPane.showMessageDialog(null,"您已成功取款"+balance+"元","取款成功",JOptionPane.INFORMATION_MESSAGE);
+                    if (Objects.equals(status, "525 OK"))
+                        JOptionPane.showMessageDialog(null, "您已成功取款" + balance + "元", "取款成功", JOptionPane.INFORMATION_MESSAGE);
                     else
-                        JOptionPane.showMessageDialog(null,"您的余额不足！","取款失败",JOptionPane.ERROR_MESSAGE);
-                } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(null, "您的余额不足！", "取款失败", JOptionPane.ERROR_MESSAGE);
+                } catch (IOException ex)
+                {
                     // 捕获并处理异常
                     ex.printStackTrace();
                 }
@@ -277,7 +306,8 @@ public class TCPClient extends JFrame
         button2_of_balance.addActionListener(new ActionListener()
         {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e)
+            {
                 setTitle("操作界面");
                 panel3.setVisible(true);
                 panel5.setVisible(false);
@@ -292,8 +322,7 @@ public class TCPClient extends JFrame
     {
 
 
-        
-        this.setSize(400,300);
+        this.setSize(400, 300);
         this.setTitle("登录界面");
         this.setLocationRelativeTo(null);
         this.setLayout(null);
@@ -322,6 +351,7 @@ public class TCPClient extends JFrame
         button1_of_balance.setFont(font);
         button2_of_balance.setFont(font);
     }
+
     public void setLayout1()
     {
         row1.add(label_of_account);
@@ -334,9 +364,9 @@ public class TCPClient extends JFrame
         col1.add(Box.createVerticalStrut(10));
         col1.add(button1);
 
-        panel1.setBounds(100,75,200,75);
+        panel1.setBounds(100, 75, 200, 75);
 
-        panel1.add(col1,BorderLayout.CENTER);
+        panel1.add(col1, BorderLayout.CENTER);
         panel1.setVisible(true);
 
         this.add(panel1);
@@ -354,9 +384,9 @@ public class TCPClient extends JFrame
         col2.add(Box.createVerticalStrut(10));
         col2.add(button2);
 
-        panel2.setBounds(100,75,200,75);
+        panel2.setBounds(100, 75, 200, 75);
 
-        panel2.add(col2,BorderLayout.CENTER);
+        panel2.add(col2, BorderLayout.CENTER);
         panel2.setVisible(false);
 
         this.add(panel2);
@@ -378,9 +408,9 @@ public class TCPClient extends JFrame
         col3.add(Box.createVerticalStrut(30));
         col3.add(row5);
 
-        panel3.setBounds(100,25,200,200);
+        panel3.setBounds(100, 25, 200, 200);
 
-        panel3.add(col3,BorderLayout.CENTER);
+        panel3.add(col3, BorderLayout.CENTER);
         panel3.setVisible(false);
 
         this.add(panel3);
@@ -399,8 +429,8 @@ public class TCPClient extends JFrame
         col4.add(Box.createVerticalStrut(30));
         col4.add(y);
 
-        panel4.setBounds(100,25,200,200);
-        panel4.add(col4,BorderLayout.CENTER);
+        panel4.setBounds(100, 25, 200, 200);
+        panel4.add(col4, BorderLayout.CENTER);
 
         panel4.setVisible(false);
 
@@ -424,11 +454,25 @@ public class TCPClient extends JFrame
         col5.add(Box.createVerticalStrut(30));
         col5.add(z);
 
-        panel5.setBounds(100,25,200,200);
-        panel5.add(col5,BorderLayout.CENTER);
+        panel5.setBounds(100, 25, 200, 200);
+        panel5.add(col5, BorderLayout.CENTER);
 
         panel5.setVisible(false);
 
         this.add(panel5);
     }
+
+    public void connect()
+    {
+        try
+        {
+            clientSocket = new Socket(ip, port);
+            writer = new DataOutputStream(clientSocket.getOutputStream());
+            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException x)
+        {
+            throw new RuntimeException(x);
+        }
+    }
+
 }
